a98d8dbcd5f50846341522bfe91ccc2b25ddc789
OPluke11-abula <luke20051026@gmail.com>
2026-03-29 20:58:18 +0800

    Complete the loading file and struct preliminary architecture

--- DS2HW2_11227141_11327229.cpp
+++ DS2HW2_11227141_11327229.cpp
@@ -11,25 +11,40 @@
 using namespace std;
 
 struct Record {
-  int id;
-  int graduates;
-  Record(int i = 0, int g = 0) : id(i), graduates(g) {}
+    int seqId;
+    string schoolId;
+    string schoolName;
+    string deptId;
+    string deptName;
+    string dayNight; 
+    string level;    
+    int students;
+    int teachers;
+    int graduates;
+    string city;
+    string type;
+
+    Record() : seqId(0), students(0), teachers(0), graduates(0) {}
 };
 
 class TwoThreeTree {
  private:
-  
+  vector<Record> heap;
  public:
   TwoThreeTree() {
     heap.reserve(1000); //配置1000個Record空間
-    heap.emplace_back(0, 0);
+  }
+
+  void PushBack(Record records_list) {
+    for (int i = 0; i < records_list.size(); ++i) {
+     heap.push_back(records_list[i]);
+    }
   }
 };
 
-bool loadFile(string filename);
+bool loadFile(const string &filename, vector<Record> &records_list);
 bool isInteger(const string &input);
 bool processCommand(int cmd);
-bool getGraduates(const string &line, int &graduates);
 
 int main() {
   while (true) {
@@ -61,28 +76,55 @@ int main() {
   return 0;
 }
 
-bool loadFile(string filename) {
-    filename = "input" + filename + ".txt";
-    ifstream file(filename);
+bool loadFile(const string &filename, vector<Record> &records_list) {
+    if (filename.empty()) {
+      cout << "\n### " << "input" + filename + ".txt" << " does not exist! ###\n";
+      return false;
+    }
+    string fullPath = "input" + filename + ".txt";
+    ifstream file(fullPath);
 
     if (!file.is_open()) {
-        cout << "\n### " << filename << " does not exist! ###" << endl;
-        return false;
+      cout << "\n### " << fullPath << " does not exist! ###\n";
+      return false;
     }
 
-    int graduates = -1;
     string line;
-    // 跳過標題列
+    // Skip 3 header lines
     getline(file, line); 
     getline(file, line);
     getline(file, line);
 
+    int seqBase = 1;
     while (getline(file, line)) {
-        if (line.empty()) continue;
+      if (line.empty()) continue;
+      if (line.back() == '\r') line.pop_back();
 
-        //TODO：
+      stringstream ss(line);
+      string token;
+      vector<string> tokens;
+      while(getline(ss, token, '\t')) {
+          tokens.push_back(token);
+      }
+        
+      while(tokens.size() < 11) tokens.push_back("");
+
+      Record r;
+      r.seqId = seqBase++;
+      r.schoolId = tokens[0];
+      r.schoolName = tokens[1];
+      r.deptId = tokens[2];
+      r.deptName = tokens[3];
+      r.dayNight = tokens[4];
+      r.level = tokens[5];
+      r.students = stoi(tokens[6]);
+      r.teachers = stoi(tokens[7]);
+      r.graduates = stoi(tokens[8]);
+      r.city = tokens[9];
+      r.type = tokens[10];
+        
+      records_list.push_back(r);
     }
-
     file.close();
     return true;
 }
@@ -101,9 +143,25 @@ bool isInteger(const string &input) {
 
 bool processCommand(int cmd) {
   string filename {};
+  static vector<Record> records_list;
 
-  if (cmd == 1 || cmd == 2) {
-    //TODO：
+  if (cmd == 1) {
+    records_list = Record();
+
+    do {
+      cout << "\nInput a file number ([0] Quit): ";
+
+      getline(cin, filename);
+      if (filename.back() == '\r') filename.pop_back();
+    
+      if (filename == "0") {
+        return true;
+      }
+    } while(!loadFile(filename, records_list));
+
+    TwoThreeTree.PushBack(records_list);
+    return true;
+  } else if (cmd == 2) {
     return true;
   }
 
